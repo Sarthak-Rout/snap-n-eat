dbd0cf57e3160c892d4781660d20edf4052ce034
Sarthak-Rout <sarthakr97@gmail.com>
2020-02-08 02:18:22 +0530

    homepage added

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,12 +1,13 @@
 import React from "react"
-import  Background from "../../assets/img/bg1.jpg"
+import  Logo from "../../assets/img/logo.png"
 import "./style.css"
 
 class LandingPage extends React.Component {
     render ()  {
         return (
             <div className="landing">
-                <button type="button" className="btn btn-light login-btn" >Smartwatch Login</button>
+                <img src={Logo} alt="logo" height="260px" width="250px"></img>
+                <button type="button" className="btn btn-light login-btn" >SMARTWATCH LOGIN</button>
             </div>
             
         )
